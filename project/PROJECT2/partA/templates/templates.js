6eb51506648be13f427bf6c293d842292ddd66a7
Jason Zhao <caitakan@qq.com>
2016-09-27 13:43:28 -0700

    Add files via upload

---
+++ project/PROJECT2/partA/templates/templates.js
@@ -0,0 +1,35 @@
+angular.module('templateStore.templates',['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/templates', {
+			templateUrl: 'templates/templates.html',
+			controller: 'TemplatesCtrl'
+		}).
+	when('/templates/:templateId', {
+		templateUrl: 'templates/template-details.html',
+		controller: 'TemplateDetailsCtrl'
+	})
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope,$routeParams, $http, $filter){
+    var templateId = $routeParams.templateId;
+    $http.get('json/templates.json').success(function (data) {
+        $scope.templates = $filter('filter')(data, function (d) {
+            return d.id == templateId;
+        })[0];
+        $scope.mainImage=$scope.templates.images[0].name;
+        console.log('$scope.templates',$scope.templates);
+    });
+    $scope.setImage = function (image) {
+        $scope.mainImage=image.name;
+
+    }
+}])
+
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
+    $http.get('json/templates.json').success(function (data) {
+        $scope.templates = data;
+        console.log('$scope.templates',$scope.templates);
+    });
+}]);
